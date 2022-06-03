9e17b1fdc9f695327f253488b24405df52cf6e7a
aurelienbrethes <aurelienbrethes@gmail.com>
2022-06-03 15:26:07 +0200

    add background + footer content + config tailwind

---
+++ src/components/Footer.tsx
@@ -0,0 +1,14 @@
+const Footer = () => {
+  return (
+    <footer className="fixed flex justify-between w-screen h-auto px-8 bottom-4">
+      <div className="flex">
+        <p className="">0752668535</p>
+        <span className="mx-8 border-l" />
+        <p className="">ab.webdesign.64@gmail.com</p>
+      </div>
+      <p>Designed and code by Aurélien Brethes</p>
+    </footer>
+  )
+}
+
+export default Footer
